8f372549beaa128535d8841e42b1470c7ca07e6a
Kuroashi1995 <kuroashi.prog@gmail.com>
2024-04-01 18:14:20 -0400

    chapter 3

---
+++ beginners-guide/chapter-3/comments.c
@@ -0,0 +1,11 @@
+/*
+This chapter was all about comments, however, there are two types of comments
+*/
+/*The ones that are like this*/
+// And the ones that are like this
+#include <stdio.h>
+int main()
+{
+    printf("THIS CHAPTER WAS ALL COMMENTS");
+    return 0;
+}
