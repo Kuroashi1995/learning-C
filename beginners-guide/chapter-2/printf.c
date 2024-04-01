9e7ccbfa84eda930d3bec7f8e5754568899ea452
Kuroashi1995 <kuroashi.prog@gmail.com>
2024-04-01 17:43:58 -0400

    initial setup and chapter 2

---
+++ beginners-guide/chapter-2/printf.c
@@ -0,0 +1,12 @@
+/*
+Comments in c are done like this
+*/
+#include <stdio.h>
+int main()
+{
+    printf("I am learning the %c programming language \n", 'C');
+    printf("I have just completed chapter %d\n", 2);
+    printf("I am %.1f percent ready to move on ", 99.9);
+    printf("to the next chapter");
+    return 0;
+}
