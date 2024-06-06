c46b03fd37a37301a8990139001fedcae9ca0696
Kuroashi1995 <kuroashi.prog@gmail.com>
2024-06-06 17:43:51 -0400

    chapter 19, doing more with strings

--- beginners-guide/chapter-19/more-string-pawa.c
+++ beginners-guide/chapter-19/more-string-pawa.c
@@ -13,24 +13,42 @@ int main()
         for (int i = 0; i < 25; i++)
         {
             password[i] = getch();
-            if (password[i] == '\r')
+            if (password[i] == '\r') // important, getch works with return character, not the newline, also, getch eats ctrl + key inputs, so it may cause trouble when trying to use ctrl + c and shit
             {
                 break;
             }
-            putch('*');
-            if (islower(password[i]))
+            putch(password[i]);
+            if (!lowercase && islower(password[i]))
             {
                 lowercase = 1;
             }
-            else if (isupper(password[i]))
+            else if (!uppercase && isupper(password[i]))
             {
                 uppercase = 1;
             }
-            else if (isdigit(password[i]))
+            else if (!number && isdigit(password[i]))
             {
                 number = 1;
             }
         }
-        printf("\ncheck: %i", lowercase && uppercase && number);
-    } while (lowercase && uppercase && number);
+        putch('\n');
+
+    } while (!(lowercase && uppercase && number));
+
+    char name[25] = "Andrew";
+    char lastName[25] = " Halley, ";
+
+    // This function declared in string.h lets us concatenate a string next to the first one
+
+    strcat(name, lastName);
+    // the compiler suggest changing strcat to strlcat, sooo lets see
+    // strlcat uses a defined length buffer char array to store the concatenated strings
+
+    // now some functions included in stdio to get and put strings
+    char job[25];
+    printf("What's your job title? ");
+    gets(job);
+    char presentation[60];
+    strcpy(presentation, strcat(name, job));
+    printf("So you are: %s", presentation);
 }
